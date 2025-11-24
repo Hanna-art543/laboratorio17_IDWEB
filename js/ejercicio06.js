e88e6007413bc09fbe1fd331fae9dce7485a06ab
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-24 16:52:53 -0500

    Subir ejercicio 06

---
+++ js/ejercicio06.js
@@ -0,0 +1,18 @@
+function validarEdad(edad) {
+    if(isNaN(edad) || edad <0) {
+        throw new Error("Edad válida");
+    }
+    return "Edad válida: " + edad;
+}
+
+function probarEdad () {
+    const out = document.getElementById("output");
+
+    try {
+        const edad = prompt ("Ingresa una edad:");
+        const resultado = validarEdad(Number(edad));
+        out.textContent = resultado;
+    } catch (e) {
+        out.textContent = e.name + ": " + e.message;
+    }
+}
